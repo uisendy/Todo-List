d0b6212a107d5eb0b6c8dbfb8af26626ef8c3e4f
Inieke Sendy <ini.e.sendy@gmail.com>
2022-08-11 18:32:48 +0100

    Add: Edit Todo

--- src/index.js
+++ src/index.js
@@ -4,23 +4,14 @@ import TodoList from "./TodoList.js";
 import UIDisplay from "./UIDisplay.js";
 import LocalStore from "./LocalStore.js";
 
-const sampleTodoList = [
-  { description: "Go on a Fishing today", completed: false, index: 0 },
-  { description: "Shopping by 3pm", completed: false, index: 1 },
-  { description: "Attend Online Class by 9pm", completed: false, index: 0 },
-];
-
 const todosDisplayContainer = document.querySelector(
   ".todos-display-container"
 );
 const todoInputForm = document.querySelector(".todo-form");
 
 const loadTodoList = () => {
-  // const todos = LocalStore.getTodos();
-  // todos.forEach((todo) => {
-  //   UIDisplay.displayTodoItem(todo);
-  // });
-  sampleTodoList.forEach((todo) => {
+  const todos = LocalStore.getTodos();
+  todos.forEach((todo) => {
     UIDisplay.displayTodoItem(todo);
   });
 };
@@ -40,29 +31,35 @@ todoInputForm.addEventListener("submit", (e) => {
   LocalStore.saveTodos(todos);
 });
 
-// todosDisplayContainer.addEventListener("change", (e) => {
-//   if (e.target.classList.contains("checkbox")) {
-//     handleCheck(e.target);
-//     e.target.parentElement.lastElementChild.classList.toggle("completed");
-//     e.target.nextElementSibling.nextElementSibling.classList.toggle(
-//       "completed"
-//     );
-//     e.target.nextElementSibling.classList.toggle("completed");
-//   }
-// });
+todosDisplayContainer.addEventListener("change", (e) => {
+  if (e.target.classList.contains("checkbox")) {
+    handleCheck(e.target);
+    e.target.parentElement.lastElementChild.classList.toggle("completed");
+    e.target.nextElementSibling.nextElementSibling.classList.toggle(
+      "completed"
+    );
+    e.target.nextElementSibling.classList.toggle("completed");
+  }
+});
 
-// const handleCheck = (index) => {
-//   const todos = LocalStore.getTodos();
-//   const checkedTodos = todos.map((todo) => {
-//     if (todo.index.toString() === index.id.toString()) {
-//       if (todo.completed === false) {
-//         index.setAttribute("checked", "");
-//       } else {
-//         index.removeAttribute("checked");
-//       }
-//       return { ...todo, completed: !todo.completed };
-//     }
-//     return todo;
-//   });
-//   LocalStore.saveTodos(checkedTodos);
-// };
+todosDisplayContainer.childNodes.forEach((todo) => {
+  todo.addEventListener("click", (e) => {});
+});
+
+const handleEditTodo = () => {};
+
+const handleCheck = (index) => {
+  const todos = LocalStore.getTodos();
+  const checkedTodos = todos.map((todo) => {
+    if (todo.index.toString() === index.id.toString()) {
+      if (todo.completed === false) {
+        index.setAttribute("checked", "");
+      } else {
+        index.removeAttribute("checked");
+      }
+      return { ...todo, completed: !todo.completed };
+    }
+    return todo;
+  });
+  LocalStore.saveTodos(checkedTodos);
+};
